1cb3c61c5f05766395e9cb0f535764de78f85939
ac118291 <ac118291@8CG8163KXX>
2022-03-11 13:59:45 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int user;
+Console.WriteLine("Enter a number: ");
+user = Convert.ToInt32(Console.ReadLine());
+if (user % 2 == 0)
+{
+    Console.WriteLine("Number is even");
+}
+else
+{
+    Console.WriteLine("Number is odd");
+}
