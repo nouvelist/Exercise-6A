8447faf48ffa28e887da1580b350aeb6c0a0b832
Grace Jarvie-Otufangavalu <ac118291@avcol.school.nz>
2022-03-11 14:05:30 +1300

    Completed exercise 6A

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿int user;
+﻿/* Grace - AC118291
+ * declare number of type int
+ * introduction
+ * prompt user for a number
+ * read number from keyboard/console
+ * check if the remainder of the number when divided by 2 is 0
+ * if true then display it is positive
+ * otherwise display it is negative
+ */
+int number;
+Console.WriteLine("This program will ask you to enter a number and it will determine whether it is odd or even\n\n\n");
 Console.WriteLine("Enter a number: ");
-user = Convert.ToInt32(Console.ReadLine());
-if (user % 2 == 0)
+number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
 {
     Console.WriteLine("Number is even");
 }
